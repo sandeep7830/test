b4796a76edd134495fd6b3d6f2522489adc1144e
sandeep7830 <64680878+sandeep7830@users.noreply.github.com>
2020-10-28 20:55:56 +0530

    final

---
+++ src/Landing.js
@@ -0,0 +1,20 @@
+import React, { Fragment } from 'react'
+
+const Landing = () => {
+    return (
+        <Fragment>
+        <section className="landing">
+  <div >
+    <div >
+      <h1 className="x-large">Landing page</h1>
+      <p className="lead">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce lorem mauris, euismod non malesuada sed, gravida et diam. Nam interdum, ante id laoreet mattis, ex orci scelerisque magna, et pellentesque leo urna vel augue. Sed sed tortor et massa consequat suscipit ac ac quam. Nunc ut aliquam eros, vel accumsan lorem.
+      </p>
+    </div>
+  </div>
+</section>
+    </Fragment>
+    )
+}
+
+export default Landing
---
+++ src/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav className="navbar bg-dark">
+      <h1>
+         navbar
+      </h1>
+      <ul>
+        <li>home</li>
+        <li>about</li>
+      </ul>
+    </nav>      
+        </div>
+    )
+}
+
+export default Navbar
